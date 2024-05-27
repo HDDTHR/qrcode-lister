74915a810f18e3688640454d9dae5e980903feb0
HDDTHR <stevenjildeh@gmail.com>
2024-05-27 14:01:45 +0300

    Add logic to write results to output_file

--- script.py
+++ script.py
@@ -8,23 +8,33 @@ from utils import Args, get_absolute_filepaths
 
 
 def run_script(args: Args):
+    logging.basicConfig(level=args.loglevel)
     qcd = cv2.QRCodeDetector()
+    result = []
 
     files = get_absolute_filepaths(args.input_directory)
     for file in files:
         img = cv2.imread(file)
 
         if img is None:
-            logging.info(f"{file} is not a valid image, Igonred.")
+            logging.warn(f"{file} is not a valid image, Igonred.")
             continue
+
+        is_qr_present, decoded_info = [False, []]
+        try:
+            is_qr_present, decoded_info, _, _ = qcd.detectAndDecodeMulti(img)
+        except Exception as e:
+            logging.warn(
+                f"{file} couldn't be processesed by the opencv2 qr code detector.")
+
+        if not is_qr_present:
+            logging.warn(f"{file} doesn't contain a QR code.")
+            continue
+
+        print(is_qr_present, decoded_info)
+
+        for txt in decoded_info:
+            if len(txt) is not 0:
+                result.append(txt)
     
-        # Threshhold the image
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(gray, 120, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-        cv2.imshow("img", thresh)
-        cv2.waitKey(0)
-
-        res = qcd.detectAndDecodeMulti(thresh)
-        print(res)
-
+    args.output_file.write('\n'.join(result))
